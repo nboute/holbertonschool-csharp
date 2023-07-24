ff6c15255608118d519324181a6c6bba01018fc2
nboute <noucoulous53@gmail.com>
2023-07-24 14:48:41 +0200

    added the rest of the mandatory assignments for today's project

---
+++ csharp-hello_world/4-print_integer/4-print_integer
@@ -0,0 +1,10 @@
+using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		int number = 972;
+		Console.WriteLine("{0} Mission Street", number);
+	}
+}
---
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+		Console.WriteLine("{0}\n{1}", percent.ToString("P"), currency.ToString("C", new CultureInfo("en-US")));
+	}
+}
