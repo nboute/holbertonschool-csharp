c755c311d6cd3e99816520a1c9696880ce89d7fd
nboute <noucoulous53@gmail.com>
2023-07-31 18:19:32 +0200

    fixed typos

--- csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -6,8 +6,10 @@ class MyStack
 {
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
-        Console.WriteLine("Number of items: " + aStack.Count);
-        Console.WriteLine("Top item: " + aStack.Peek());
+        var count = aStack.Count;
+        Console.WriteLine("Number of items: " + count);
+        if (count > 0)
+            Console.WriteLine("Top item: " + aStack.Peek());
         bool contains = aStack.Contains(search);
         Console.WriteLine("Stack contains \"{0}\": {1}", search ,contains);
         while (aStack.Contains(search))
