cf0a1847830c4492265abbf8f7ffbde8da9bd719
niboute <noucoulous53@gmail.com>
2023-08-14 13:32:45 +0200

    added assignments for projet csharp-exceptions

---
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+	public static int SafePrint(List<int> myList, int n)
+	{
+		int printed = 0;
+		try
+		{
+			for (int i = 0; i < n; i++)
+			{
+				Console.WriteLine(myList[i]);
+				printed++;
+			}
+		} catch (ArgumentOutOfRangeException)
+		{
+		}
+		return printed;
+	}
+}
---
+++ csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Int
+{
+	public static void divide(int a, int b)
+	{
+		int res = 0;
+		try
+		{
+			res = a / b;
+		}
+		catch (DivideByZeroException)
+		{
+			Console.WriteLine("Cannot divide by zero");
+		}
+		finally
+		{
+			Console.WriteLine($"{a} / {b} = {res}");
+		}
+	}
+}
