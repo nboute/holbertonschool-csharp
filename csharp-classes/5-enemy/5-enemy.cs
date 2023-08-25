d1a2e3854f15b0b9a37cda1a4c9a4926e69b39f1
niboute <noucoulous53@gmail.com>
2023-08-25 21:07:55 +0200

    reverted previous commit

--- csharp-classes/5-enemy/5-enemy.cs
+++ csharp-classes/5-enemy/5-enemy.cs
@@ -51,7 +51,7 @@
 		/// <summary>
 		/// Returns a string representation of the Zombie instance
 		/// </summary>
-		public override string toString()
+		public override string ToString()
 		{
 			return "Zombie name: " + name + " / Total Health: " + health;
 		}
