41a001198787480ed7407d34a86c5b11e3788fbd
Nicolas Bouté <nicolasboute123@gmail.com>
2023-09-17 14:19:25 +0200

    added final assignment for linear algerabra project

---
+++ csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+/// <summary>
+/// Contains methods for matrix math.
+/// </summary>
+class MatrixMath
+{
+    /// <summary>
+    /// Calculates the inverse of a 2D matrix.
+    /// </summary>
+    /// <param name="matrix"></param>
+    /// <returns></returns>
+    public static double[,] Inverse2D(double[,] matrix)
+    {
+        double[,] inverse = new double[2, 2];
+        double det = 0;
+
+        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+        {
+            return new double[,] { { -1 } };
+        }
+
+        det = matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
+
+        if (det == 0)
+        {
+            return new double[,] { { -1 } };
+        }
+
+        inverse[0, 0] = Math.Round(matrix[1, 1] / det, 2);
+        inverse[0, 1] = Math.Round(-matrix[0, 1] / det, 2);
+        inverse[1, 0] = Math.Round(-matrix[1, 0] / det, 2);
+        inverse[1, 1] = Math.Round(matrix[0, 0] / det, 2);
+
+        return inverse;
+    }
+}
