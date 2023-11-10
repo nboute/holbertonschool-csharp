0484b9ea65c10e0f14dc4e51aa8dbf5be12f0202
nboute <noucoulous53@gmail.com>
2023-11-10 14:40:13 +0100

    added required file for each assignment

---
+++ csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/// <summary>
+/// Represents a queue.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+class Queue<T>
+{
+    /// <summary>
+    /// method that returns the Queue's type
+    /// </summary>
+    /// <returns></returns>
+    public string CheckType()
+    {
+        return typeof(T).ToString();
+    }
+
+
+}
+
