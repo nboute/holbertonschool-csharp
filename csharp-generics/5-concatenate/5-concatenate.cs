47dc120b86de42240b9d87b5ad284888108a9df3
nboute <noucoulous53@gmail.com>
2023-11-10 12:28:55 +0100

    added new project

---
+++ csharp-generics/5-concatenate/5-concatenate.cs
@@ -0,0 +1,169 @@
+﻿using System;
+
+/// <summary>
+/// Represents a queue.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+class Queue<T>
+{
+	/// <summary>
+	/// head of the queue
+	/// </summary>
+	Node head;
+	/// <summary>
+	/// tail of the queue
+	/// </summary>
+	Node tail;
+	/// <summary>
+	/// number of nodes in the queue
+	/// </summary>
+	int count = 0;
+
+
+	/// <summary>
+	/// Represents a node of a queue.
+	/// </summary>
+	public class Node
+	{
+		/// <summary>
+		/// value of the node
+		/// </summary>
+		public T value = default(T);
+		/// <summary>
+		/// next node in the queue
+		/// </summary>
+		public Node next = null;
+
+		/// <summary>
+		/// Constructor for a Node.
+		/// </summary>
+		/// <param name="value"></param>
+		public Node(T value)
+		{
+			this.value = value;
+		}
+	}
+
+	/// <summary>
+	/// method that returns the Queue's type
+	/// </summary>
+	/// <returns></returns>
+	public string CheckType()
+	{
+		return typeof(T).ToString();
+	}
+
+	/// <summary>
+	/// Creates a new Node and adds it to the end of the queue.
+	/// </summary>
+	public void Enqueue(T value)
+	{
+		Node newNode = new Node(value);
+		if (head == null)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			tail.next = newNode;
+			tail = newNode;
+		}
+		count++;
+	}
+
+	/// <summary>
+	/// Removes the first node in the queue and returns its value.
+	/// </summary>
+	/// <returns></returns>
+	public T Dequeue()
+	{
+		if (head == null)
+		{
+			Console.WriteLine("Queue is empty");
+			return default(T);
+		}
+		else
+		{
+			T value = head.value;
+			head = head.next;
+			count--;
+			return value;
+		}
+	}
+
+	/// <summary>
+	/// Returns the value of the first node of the queue without removing the node.
+	/// </summary>
+	/// <returns></returns>
+	public T Peek()
+	{
+		if (head == null)
+		{
+			Console.WriteLine("Queue is empty");
+			return default(T);
+		}
+		else
+		{
+			return head.value;
+		}
+	}
+
+	/// <summary>
+	/// Prints the queue, starting from the head.
+	/// </summary>
+	/// <returns></returns>
+	public string Concatenate()
+	{
+		if (head == null)
+		{
+			Console.WriteLine("Queue is empty");
+			return null;
+		}
+		if (typeof(T) != typeof(string) && typeof(T) != typeof(char))
+		{
+			Console.WriteLine("Concatenate() is for a queue of Strings or Chars only.");
+			return null;
+		}
+		string concat = "";
+		Node current = head;
+		while (current != null)
+		{
+			concat += current.value;
+			if (typeof(T) == typeof(string) && current.next != null)
+				concat += " ";
+			current = current.next;
+		}
+		return concat;
+	}
+
+	/// <summary>
+	/// Returns the number of nodes in a queue.
+	/// </summary>
+	/// <returns></returns>
+	public int Count()
+	{
+		return count;
+	}
+
+	/// <summary>
+	/// Prints the queue, starting from the head.
+	/// </summary>
+	public void Print()
+	{
+		if (head == null)
+		{
+			Console.WriteLine("Queue is empty");
+		}
+		else
+		{
+			Node current = head;
+			while (current != null)
+			{
+				Console.WriteLine(current.value);
+				current = current.next;
+			}
+		}
+	}
+
+}
---
+++ csharp-generics/README.md
@@ -0,0 +1,13 @@
+# C# - Generics
+
+## Learning Objectives
+At the end of this project, you are expected to be able to explain to anyone, without the help of Google:
+
+### General
+- What are generics and what are their purpose
+- What common generic classes and interfaces are provided in the .NET class library
+- When and how to create generic classes
+- When and how to create generic methods
+- How access modifiers affect a class and its members
+- What is the default(T) expression used for
+- What is covariance and contravariance
